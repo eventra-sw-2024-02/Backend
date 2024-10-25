d80660270c531a6dac476b568cad298129ee0841
GustavoSiancas <u202020594@upc.edu.pe>
2024-10-25 06:44:27 -0500

    add swagger
    thx good

--- src/main/java/com/example/demo/controller/client/ClientController.java
+++ src/main/java/com/example/demo/controller/client/ClientController.java
@@ -11,27 +11,24 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/clients") // Base path para los clientes
+@RequestMapping("/api/clients") // Base path para los clientes
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
 
-    // Endpoint para crear un nuevo cliente
     @PostMapping
     public ResponseEntity<ClientEntity> createClient(@RequestBody ClientRequest clientRequest) {
         ClientEntity createdClient = clientService.createClient(clientRequest);
         return ResponseEntity.ok(createdClient);
     }
 
-    // Endpoint para obtener todos los clientes
     @GetMapping
     public ResponseEntity<List<ClientEntity>> getAllClients() {
         List<ClientEntity> clients = clientService.getAllClients();
         return ResponseEntity.ok(clients);
     }
 
-    // Endpoint para obtener un cliente por su ID
     @GetMapping("/{id}")
     public ResponseEntity<ClientEntity> getClientById(@PathVariable Long id) {
         Optional<ClientEntity> client = clientService.getClientById(id);
